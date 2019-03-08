03e9a11c40058e5ac59a8285e3395237f74c3a8c
zhaohaomin <123@123.com>
2019-03-09 00:16:15 +0800

    添加“写博客”功能

--- js/blogManagement.js
+++ js/blogManagement.js
@@ -1,9 +1,16 @@
 var localhost = "http://localhost:8080";
+var editIndex = '';
+layui.use(['layer', 'layedit', 'jquery'], function() {});
 var app = new Vue({
 	el: '#app',
 	data: {
 		listData: [],
-		title: ''
+		searchTitle: '',
+		addTitle: '',
+		addTag: '',
+		addCategory: '',
+		addContent: '',
+		selected: ''
 	},
 	methods: {
 		getPage: function(n) {
@@ -15,38 +22,118 @@ var app = new Vue({
 					elem: 'pages',
 					count: app.listData.total, //数据总数
 					limit: app.listData.size, //每页显示的条数。laypage将会借助 count 和 limit 计算出分页数。、
-					jump: function(obj, first) {
-						//obj包含了当前分页的所有参数：
-						
+					jump: function(obj, first) { //obj包含了当前分页的所有参数：
 						//首次不执行
 						if (!first) {
 							app.getData(obj.curr);
-							
 						}
 					}
 				});
 			});
-
 		},
 		getData: function(n) {
 			var _this = this;
-			console.log(_this.title);
 			Vue.http.get(localhost + '/blog/list/' + n, {
 				params: {
 					pageSize: 5,
-					title: _this.title
+					title: _this.searchTitle
 				}
 			}).then(function(res) {
 				_this.listData = res.body.data;
 				console.log(app);
-					
-				if(n == 1){
+
+				if (n == 1) {
 					_this.getPage();
 				}
-				
+
 			}, function() {
 				console.log('获取博客信息失败！');
 			});
+		},
+		addBlog: function() {
+			layui.use(['layer', 'layedit', 'jquery'], function() {
+				var layer = layui.layer;
+				var layedit = layui.layedit;
+				layer.open({
+					type: 1,
+					maxmin: true,
+					area: '800px',
+					// offset: '100px',
+					title: '添加新的博客',
+					content: layui.$('#formbox'),
+					success: function(layero, index) {
+						layui.form.render();
+						layedit.set({
+							uploadImage: {
+								url: localhost + '/admin/richtext_img_upload' //接口url
+								//type:'get'
+							}
+						});
+						editIndex = layedit.build('add');
+					},
+					btn: ['确认', '取消'],
+					yes: function(index, layero) {
+						//按钮【按钮一】的回调
+						app.submitAdd(index);
+					}
+				});
+			});
+		},
+		submitAdd: function(index) {
+			console.log(app.addContent);
+			if (app.checkTitle() && app.checkTag() && app.checkCategory() && app.checkContent()) {
+				Vue.http.post(localhost + '/admin/add', {
+					title: app.addTitle,
+					category: layui.$('#addCategory').val(),
+					context: layui.layedit.getContent(editIndex),
+					tags: app.addTag
+				}, {
+					emulateJSON: true
+				}).then(function(res) {
+					if (res.body.code == 200) {
+						layui.layer.msg(res.body.data + '!', {
+							icon: 1
+						});
+						layui.layer.close(index);
+						app.addTitle = app.addTag = app.addContent = '';
+						layui.$('#addCategory').val('');
+					};
+				}, function() {
+					console.log('获取博客信息失败！');
+				});
+			}
+		},
+		checkTitle: function() {
+			if (app.addTitle == '') {
+				layui.layer.msg("标题不能为空！");
+				return false;
+			} else {
+				return true;
+			}
+		},
+		checkTag: function() {
+			if (app.addTag == '') {
+				layui.layer.msg("标签不能为空！");
+				return false;
+			} else {
+				return true;
+			}
+		},
+		checkCategory: function() {
+			if (layui.$('#addCategory').val() == '') {
+				layui.layer.msg("分类不能为空！");
+				return false;
+			} else {
+				return true;
+			}
+		},
+		checkContent: function() {
+			if (layui.layedit.getContent(editIndex) == '') {
+				layui.layer.msg("内容不能为空！");
+				return false;
+			} else {
+				return true;
+			}
 		}
 
 	}
